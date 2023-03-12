d0dd21a9048da0abeafc9e12c647cde22253dae6
Jakub Kacálek <kacalek.kuba@post.cz>
2023-03-12 19:37:42 +0100

    connect mysql

---
+++ shopping-list/src/main/resources/static/precache-manifest.e5e4ac4802f1ec112413da16295120f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5ef92f06ec66325df552",
+    "url": "/static/js/main.5ef92f06.chunk.js"
+  },
+  {
+    "revision": "76ab17af3b2ee8839b42",
+    "url": "/static/js/1.76ab17af.chunk.js"
+  },
+  {
+    "revision": "5ef92f06ec66325df552",
+    "url": "/static/css/main.f279bdf4.chunk.css"
+  },
+  {
+    "revision": "b7c0e41b102e4cea5ef8246ea7cd281c",
+    "url": "/index.html"
+  }
+];
